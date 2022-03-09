2b0df53e26681906de8785cf57309fe90a7b90f3
Ghost Chain <ghost@cashio.app>
2022-03-09 17:50:07 -0500

    fix peer dependencies

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,10 +1,10 @@
-import { Coder } from "@project-serum/anchor";
+import { BorshCoder } from "@project-serum/anchor";
 import type { KeyedAccountInfo } from "@solana/web3.js";
 
 import type { BankData, CollateralData } from ".";
 import { BankmanJSON } from "./idls/bankman";
 
-export const BANKMAN_CODER = new Coder(BankmanJSON);
+export const BANKMAN_CODER = new BorshCoder(BankmanJSON);
 
 export const parseBank = (data: KeyedAccountInfo): BankData =>
   BANKMAN_CODER.accounts.decode<BankData>("Bank", data.accountInfo.data);
